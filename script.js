bb12f49dc9ad03104fa6086160d98a2a3b08d991
Paula <pjstudium@outlook.de>
2026-03-12 17:57:56 +0100

    Bilderliste evrvollständigt

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const puzzleData = [
     { front: '1v.jpeg', back: '1h.jpeg' },
-    { front: '2v.jpeg', back: '2h.jpeg' }
+    { front: '2v.jpeg', back: '2h.jpeg' },
+    { front: '3v.jpeg', back: '3h.jpeg' },
+    { front: '4v.jpeg', back: '4h.jpeg' },
+    { front: '5v.jpeg', back: '5h.jpeg' },
+    { front: '6v.jpeg', back: '6h.jpeg' },
+    { front: '7v.jpeg', back: '7h.jpeg' },
+    { front: '8v.jpeg', back: '8h.jpeg' },
+    { front: '9v.jpeg', back: '9h.jpeg' },
+    { front: '10v.jpeg', back: '10h.jpeg' },
+    { front: '11v.jpeg', back: '11h.jpeg' },
+    { front: '12v.jpeg', back: '12h.jpeg' },
+    { front: '13v.jpeg', back: '13h.jpeg' },
+    { front: '14v.jpeg', back: '14h.jpeg' },
+    { front: '15v.jpeg', back: '15h.jpeg' },
+    { front: '16v.jpeg', back: '16h.jpeg' },
+    { front: '17v.jpeg', back: '17h.jpeg' },
+    { front: '18v.jpeg', back: '18h.jpeg' },
+    { front: '19v.jpeg', back: '19h.jpeg' }
 ];
 
 let currentIndex = 0;
